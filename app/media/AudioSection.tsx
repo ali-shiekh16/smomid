c3e34392db4d8532bc84dad7181834c1a6a4a5b1
Ali <ali.shiekh16@outlook.com>
2025-03-21 02:45:03 +0500

    Embed sound cloud.

--- app/media/AudioSection.tsx
+++ app/media/AudioSection.tsx
@@ -8,14 +8,19 @@ const AudioSection = () => {
   return (
     <Section>
       <HeadingUnderlined>Audio Section</HeadingUnderlined>
-      <Nblock className='my-20'>
-        <Image
-          className='object-contain w-full h-full'
-          src='/images/audio-player.png'
-          alt='Audio'
-          width={1024}
-          height={500}
-        />
+      <Nblock className='my-20 space-y-15'>
+        <iframe
+          width='100%'
+          height='300'
+          allow='autoplay'
+          src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/648941351&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+        ></iframe>
+        <iframe
+          width='100%'
+          height='300'
+          allow='autoplay'
+          src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/287146115&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+        ></iframe>
       </Nblock>
     </Section>
   );
---
+++ app/media/masonary.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Masonary = () => {
+  return <div>Masonary</div>;
+};
+
+export default Masonary;
