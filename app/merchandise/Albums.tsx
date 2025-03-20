65532d96d61ba1dc0896aaf0073788e7318d3cb7
Ali <ali.shiekh16@outlook.com>
2025-03-21 02:25:10 +0500

    Add merchandise links.

--- app/merchandise/Albums.tsx
+++ app/merchandise/Albums.tsx
@@ -3,32 +3,38 @@ import HeadingUnderlined from '../components/HeadingUnderlined';
 import Section from '../components/Section';
 import ProductCard from './ProductCard';
 import ButtonOutline from '../components/ButtonOutline';
+import Link from 'next/link';
 
 const data = [
   {
     title: 'Cyber Solstice',
     price: '$8 USD or more',
     image: '/images/albums/1.png',
+    link: 'https://smomid.bandcamp.com/album/cyber-solstice',
   },
   {
     title: 'Pyramidi Scheme',
     price: '$7 USD or more',
     image: '/images/albums/2.png',
+    link: 'https://smomid.bandcamp.com/album/pyramidi-scheme',
   },
   {
     title: 'Smoment In Time',
     price: '$7 USD or more',
     image: '/images/albums/3.png',
+    link: 'https://smomid.bandcamp.com/album/smoment-in-time',
   },
   {
     title: 'Rhythms Of Light',
     price: '$7 USD or more',
     image: '/images/albums/4.png',
+    link: 'https://smomid.bandcamp.com/album/rhythms-of-light',
   },
   {
     title: 'Yule Need This',
     price: '$7 USD or more',
     image: '/images/albums/5.png',
+    link: 'https://smomid.bandcamp.com/album/yule-need-this',
   },
 ];
 
@@ -37,21 +43,22 @@ const Albums = () => {
     <Section>
       <HeadingUnderlined className='uppercase'>Albums</HeadingUnderlined>
       <div className='flex flex-wrap justify-center space-x-8 space-y-15 my-20'>
-        {data.map((album, index) => (
-          <ProductCard
-            key={index}
-            title={album.title}
-            price={album.price}
-            image={album.image}
-          />
+        {data.map(album => (
+          <Link href={album.link} key={album.link}>
+            <ProductCard
+              title={album.title}
+              price={album.price}
+              image={album.image}
+            />
+          </Link>
         ))}
       </div>
 
-      <div className='flex justify-center mt-28'>
+      {/* <div className='flex justify-center mt-28'>
         <ButtonOutline className='w-fit rounded-none px-6 py-3 bg-black/15'>
           <span className='text-2xl'>Show More</span>
         </ButtonOutline>
-      </div>
+      </div> */}
       {/* <Image
         className='w-full object-cover my-20 '
         src='/images/merchandise-albums.png'
--- app/merchandise/ShirtsGrid.tsx
+++ app/merchandise/ShirtsGrid.tsx
@@ -4,6 +4,7 @@ import ProductCard from './ProductCard';
 import ButtonOutline from '../components/ButtonOutline';
 import HeadingUnderlined from '../components/HeadingUnderlined';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const ShirtsGrid = () => {
   return (
@@ -13,22 +14,24 @@ const ShirtsGrid = () => {
       </HeadingUnderlined>
       <div className='flex flex-col my-20 mx-20'>
         <p className='text-2xl mb-2'>S,M,L,XL,XXL</p>
-        <div className='flex border-1 border-white p-5 space-x-10'>
-          <Image
-            className='object-contain w-full h-full'
-            src='/images/shirts/1.png'
-            alt='Shirts'
-            width={500}
-            height={500}
-          />
-          <Image
-            className='object-contain w-full h-full'
-            src='/images/shirts/2.png'
-            alt='Shirts'
-            width={500}
-            height={500}
-          />
-        </div>
+        <Link href='https://smomid.bandcamp.com/merch/smomid-ssri-crew-shirt-limited-edition'>
+          <div className='flex border-1 border-white p-5 space-x-10 cursor-pointer'>
+            <Image
+              className='object-contain w-full h-full'
+              src='/images/shirts/1.png'
+              alt='Shirts'
+              width={500}
+              height={500}
+            />
+            <Image
+              className='object-contain w-full h-full'
+              src='/images/shirts/2.png'
+              alt='Shirts'
+              width={500}
+              height={500}
+            />
+          </div>
+        </Link>
         <div className='flex justify-between mt-2'>
           <p className='text-2xl'>Smomid - SSRI Crew Shirt, Limited Edition</p>
           <p className='text-2xl'>$16 USD or more</p>
