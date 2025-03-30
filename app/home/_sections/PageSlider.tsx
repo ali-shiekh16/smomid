6a4005c0c2ed16de3f36582dab18719136002ced
Ali <ali.shiekh16@outlook.com>
2025-03-30 07:21:07 +0500

    Redefine styles for bullets.

--- app/home/_sections/PageSlider.tsx
+++ app/home/_sections/PageSlider.tsx
@@ -32,12 +32,14 @@ export default function PageSlider() {
   return (
     <div className='relative flex'>
       {/* Sidebar Bullets */}
-      <div className='fixed left-5 top-1/2 -translate-y-1/2 flex flex-col gap-4'>
+      <div className='fixed left-10 top-1/2 -translate-y-1/2 flex flex-col gap-4'>
         {sections.map((_, index) => (
           <div
             key={index}
-            className={`w-4 h-4 rounded-full transition-all duration-300 ${
-              activeIndex === index ? 'bg-blue-500 scale-125' : 'bg-gray-300'
+            className={`w-3 h-3 rounded-full transition-all duration-300 ${
+              activeIndex === index
+                ? 'border-1 bg-blue-500 scale-125'
+                : 'bg-gray-300'
             }`}
           ></div>
         ))}
