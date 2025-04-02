a2e49538b5ac857b708c4856f6df6d644bd45d92
Ali <ali.shiekh16@outlook.com>
2025-04-02 22:01:08 +0500

    Add padding in merchandise section home page.

--- app/home/_sections/Merchandise.tsx
+++ app/home/_sections/Merchandise.tsx
@@ -18,7 +18,7 @@ const data = [
   },
   {
     title: 'Albums',
-    image: '/images/albums/1.png',
+    image: '/images/media/37.webp',
     link: '/merchandise',
   },
 ];
@@ -43,7 +43,7 @@ const Merchandise = () => {
 
   return (
     <Section>
-      <div ref={container}>
+      <div ref={container} className='px-32'>
         <div className='flex ml-auto mb-10 justify-between border-1 border-white self-end w-1/3'>
           <div className='text-xl bg-transparent text-white outline-none border-none p-2 '>
             <Link href='/merchandise' className='cursor-pointer'>
