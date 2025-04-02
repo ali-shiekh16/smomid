af17a48997a6cb7c35b683e3cc6a0f457f0ab3ac
Ali <ali.shiekh16@outlook.com>
2025-04-02 22:52:42 +0500

    Add padding to the Audio Section home page.

--- app/media/AudioSection.tsx
+++ app/media/AudioSection.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import Section from '../components/Section';
 import HeadingUnderlined from '../components/HeadingUnderlined';
-import Image from 'next/image';
-import Nblock from '../components/Nblock';
 
 const AudioSection = () => {
   return (
-    <Section>
+    <Section className='space-y-10'>
       <HeadingUnderlined>Audio</HeadingUnderlined>
-      <Nblock className='my-20 space-y-15'>
-        <iframe
-          width='100%'
-          height='300'
-          allow='autoplay'
-          src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/648941351&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
-        ></iframe>
-        <iframe
-          width='100%'
-          height='300'
-          allow='autoplay'
-          src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/287146115&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
-        ></iframe>
-      </Nblock>
+      <iframe
+        width='100%'
+        height='300'
+        allow='autoplay'
+        src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/648941351&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+      ></iframe>
+      <iframe
+        width='100%'
+        height='300'
+        allow='autoplay'
+        src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/287146115&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+      ></iframe>
     </Section>
   );
 };
