60be8a8cd1b4c457e7dc524313146a1b6a7de65b
Ali <ali.shiekh16@outlook.com>
2025-04-04 22:07:41 +0500

    Add links to social media icons on contact page.

--- app/contact/SocialMedia.tsx
+++ app/contact/SocialMedia.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
 import Section from '../components/Section';
+import Link from 'next/link';
 
 const SocialMedia = () => {
-  const socials = ['instagram', 'facebook', 'bandcamp', 'youtube', 'spotify'];
+  const socials = [
+    { icon: 'instagram', href: 'https://www.instagram.com/smomid/' },
+    { icon: 'facebook', href: 'https://www.facebook.com/Smomid' },
+    { icon: 'bandcamp', href: 'https://smomid.bandcamp.com/' },
+    { icon: 'youtube', href: 'https://www.youtube.com/@Smomid' },
+    {
+      icon: 'spotify',
+      href: 'https://open.spotify.com/artist/34tsz5yU9BVkcSjlZvzP2W',
+    },
+  ];
 
   return (
     <Section>
@@ -16,18 +26,25 @@ const SocialMedia = () => {
       />
       <div className='md:flex md:justify-between md:items-center my-15 md:space-y-0 space-y-10'>
         <h2 className='text-center md:text-left text-4xl md:text-5xl font-neo-latina font-bold'>
-          {/* Connect with <br /> us on <br /> Social media */}
+          Connect with <br /> us on <br /> Social media
         </h2>
         <div className='grid place-content-center grid-cols-3 grid-rows-2 gap-5'>
-          {socials.map(icon => (
-            <div className='bg-[#0a2a7e] rounded-2xl p-3'>
-              <Image
-                src={`/icons/${icon}.svg`}
-                alt='Logo.'
-                width='60'
-                height='60'
-              />
-            </div>
+          {socials.map(({ icon, href }) => (
+            <Link
+              key={icon}
+              href={href}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <div className='bg-[#0a2a7e] rounded-2xl p-3'>
+                <Image
+                  src={`/icons/${icon}.svg`}
+                  alt={`${icon} Logo`}
+                  width='60'
+                  height='60'
+                />
+              </div>
+            </Link>
           ))}
         </div>
       </div>
