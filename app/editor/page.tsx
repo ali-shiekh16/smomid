d26ee78a2e687ae7ed568b1b5fee0f7526b890bb
Ali <ali.shiekh16@outlook.com>
2025-04-13 04:04:44 +0500

    Add tiptap editor.

---
+++ app/editor/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TipTap from '../components/TipTap';
+import Section from '../components/Section';
+
+const Page = () => {
+  return (
+    <Section>
+      <TipTap />
+    </Section>
+  );
+};
+
+export default Page;
