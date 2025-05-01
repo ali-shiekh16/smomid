f4540eb9a9627ed3a50e210f10b5ee3a579864c2
Ali <ali.shiekh16@outlook.com>
2025-05-01 11:39:38 +0500

    Fix merchandise heading issue.

--- app/merchandise/MerchandiseCover.tsx
+++ app/merchandise/MerchandiseCover.tsx
@@ -4,27 +4,10 @@ import Section from '../components/Section';
 const MerchandiseCover = () => {
   return (
     <Section>
-      <h1 className='text-5xl text-center md:text-left md:text-9xl my-15 md:my-20 font-bold uppercase font-neo-latina'>
+      <h1 className='text-[10vw] text-center md:text-left md:text-9xl my-15 md:my-20 font-bold uppercase font-neo-latina'>
         Merchandise
       </h1>
     </Section>
-    // <Nblock>
-    //   <header className='relative bg-[url(/images/merchandise-cover.png)] bg-contain bg-bottom bg-no-repeat h-screen'>
-    //     <Block>
-    //       <h1 className='absolute top-[20%] text-[10vw] font-bold text-white uppercase font-neo-latina'>
-    //         Merchandise
-    //       </h1>
-    //     </Block>
-    //   </header>
-    //   <Image
-    //     className='w-full object-contain mt-10'
-    //     src='/images/merchandise-marquee.png'
-    //     width='1920'
-    //     height='400'
-    //     alt='Merchandise Marquee'
-    //     objectFit='contain'
-    //   />
-    // </Nblock>
   );
 };
 
